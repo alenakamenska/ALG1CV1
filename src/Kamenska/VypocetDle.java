b0dee39ab6f120672d365ffafe9cc1d7459a12e6
Kamenska-A-dcf0 <Kamenska-A-dcf0@TK6-007-mti.skynet.tul.cz>
2023-09-21 12:14:02 +0200

    Reseni zadanyvh uloh

---
+++ src/Kamenska/VypocetDle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Kamenska;
+
+/**
+ *
+ * @author Kamenska-A-dcf0
+ */
+public class VypocetDle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("zadej vstupy");
+        System.out.println("provadim");
+    }
+    
+}
